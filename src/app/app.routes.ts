b2977ff75a574a679e34fbe95e041b5f334793a9
Zeitoum <gabrielzeitoum@hotmail.com>
2025-09-25 17:05:30 -0300

    Revert "Alterando nome do mapa e explicando melhor como ler o mapa"
    This reverts commit 7d509e8f48acbfb636d88a327a3a9b5d0f49afe4.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import {Home} from './home/home'
-import { ProcessosApi } from './processos-api/processos-api';
 
-export const routes: Routes = [
-  {path: '', component: Home},
-  {path: 'processosApi', component: ProcessosApi},
-];
+export const routes: Routes = [];
